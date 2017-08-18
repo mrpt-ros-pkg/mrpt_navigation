b22badb42bbd27c43690f7565799626a68525438
Raghavender Sahdev <raghavendersahdev@gmail.com>
2017-08-18 12:43:31 -0400

    changed test_Bridge file, testing the bridge in a different way

--- mrpt_bridge/src/test/test_Bridge.cpp
+++ mrpt_bridge/src/test/test_Bridge.cpp
@@ -3,35 +3,21 @@
 //
 
 #include "mrpt_bridge/GPS.h"
+#include "mrpt_bridge/range.h"
+#include "mrpt_bridge/imu.h"
+#include "mrpt_bridge/image.h"
+
 #include "ros/ros.h"
 
 /// mrpt imports
-#include <dirent.h>
 #include <mrpt/obs/CObservation.h>
-#include <mrpt/utils/CMemoryStream.h>
-#include <mrpt/utils/metaprogramming.h>
-#include <mrpt/math/data_utils.h>
-#include <mrpt/vision/tracking.h>
-#include <mrpt/system/os.h>
-#include <mrpt/vision/CFeatureExtraction.h>
 #include <mrpt/obs/CRawlog.h>
 #include <mrpt/obs.h>
 #include <mrpt/obs/CObservationImage.h>
 #include <mrpt/obs/CObservationStereoImages.h>
-#include <mrpt/utils/CFileGZInputStream.h>
-#include <mrpt/obs/CActionCollection.h>
-#include <mrpt/obs/CSensoryFrame.h>
-#include <mrpt/obs/CAction.h>
 #include <mrpt/system/filesystem.h>
-#include <mrpt/obs/CObservation3DRangeScan.h>
-#include <mrpt/obs/CSensoryFrame.h>
-#include <mrpt/obs/CRawlog.h>
+#include <mrpt/obs/CObservationRange.h>
 
-
-#include <boost/interprocess/sync/scoped_lock.hpp>
-#include <iostream>
-#include <memory>
-#include <mrpt/version.h>
 
 ///c++ standard stuff
 #include <stdio.h>
@@ -48,13 +34,17 @@
 #include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
+
+#include <sensor_msgs/NavSatFix.h>
+#include <sensor_msgs/Range.h>
+#include <sensor_msgs/Imu.h>
+
+
+using namespace sensor_msgs;
 using namespace mrpt::obs;
 using namespace mrpt::system;
-using namespace mrpt::vision;
 using namespace mrpt::utils;
-using namespace mrpt::math;
 using namespace mrpt;
-using namespace mrpt::poses;
 
 using namespace std;
 using namespace cv;
@@ -62,19 +52,7 @@ using namespace cv;
 
 int rawlog_type = 0;
 
-string getImageDir(string path)
-{
-    long i = path.size()-1;
-    for(; i>=0 ; i--)
-    {
-        if(path.at(i) == '/')
-        {
-            break;
-        }
-    }
-    string temp = path.substr(0,i);;
-    return temp;
-}
+
 
 
 int main(int argc, char **argv)
@@ -82,104 +60,28 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "talker");
     ros::NodeHandle n;
 
+    Imu     message1 ;
+    sensor_msgs::Range   message2;
 
-    /// APPROACH 2
-    string rawlog = "/home/raghavender/Desktop/GSoC/datasets/malaga-urban-dataset-extract-01/malaga-urban-dataset-extract-01_all-sensors.rawlog";
-    CRawlog dataset;
-    dataset.loadFromRawLogFile(rawlog);
-    cout << dataset.size() << " entries loaded." << endl;
+    CObservationGPS     msg1;
+    CObservationIMU     msg2;
+    CObservationRange   msg3;
 
+    msg3.maxSensorDistance = 212;
+    msg3.minSensorDistance = 212;
+    msg3.sensorConeApperture =33;
 
-    string errorMsg;
-    ofstream ostream1;
+    std_msgs::Header header;
+    header.frame_id = 23;
 
-    for(unsigned int i=0 ; i<dataset.size() ; i++)
-    {
-        try
-        {
-            switch (dataset.getType(i))
-            {
-                case CRawlog::etObservation:
-                {
-                    cout << "etObservation " << i << endl;
-                    CObservation::Ptr o = dataset.getAsObservation(i);
-                    //cout << o->sensorLabel << " type : etObs" << dataset.getType(i) << " index: " << i << endl;
+    //mrpt_bridge::range::mrpt2ros(msg3, header, message2);
+    //mrpt_bridge::imu::mrpt2ros(msg2, header, message1);
 
-                    if (IS_CLASS(o, CObservationStereoImages))
-                    {
-                        cout << " stereo Image detected " << endl;
-                        CObservationStereoImages::Ptr obsSt = std::dynamic_pointer_cast<CObservationStereoImages>(o);
+    ROS_INFO("conversion successful");
 
-                        ///similar to common practice of reading images-->  https://github.com/MRPT/mrpt/search?utf8=%E2%9C%93&q=IMAGES_PATH_BASE&type=
-                        stringstream str;
-                        str << getImageDir(rawlog) << "/Images";
-                        obsSt->imageLeft.IMAGES_PATH_BASE = str.str();
-
-                        cout << obsSt->imageLeft.getExternalStorageFileAbsolutePath() << " external " << endl;
-
-                        rawlog_type = 1;
-
-                        CImage image_c = obsSt->imageLeft;
-                        Mat cvImg1 = cv::cvarrToMat(image_c.getAs<IplImage>());
-                        //imshow("view", cvImg1);
-                        //waitKey(1);
-                    }
-                    else if (IS_CLASS(o, CObservationImage))
-                    {
-                        cout << "monocular image detected " << endl;
-                        CObservationImage::Ptr obsIm = std::dynamic_pointer_cast<CObservationImage>(o);
-
-                        ///similar to common practice of reading images-->  https://github.com/MRPT/mrpt/search?utf8=%E2%9C%93&q=IMAGES_PATH_BASE&type=
-                        stringstream str;
-                        str << getImageDir(rawlog) << "/Images";
-                        obsIm->image.IMAGES_PATH_BASE = str.str();
-
-                    }
-                    else if (IS_CLASS(o, CObservationGPS))
-                    {
-                        /// GPS reading and publishing happens here
-                        CObservationGPS::Ptr obsGPS = std::dynamic_pointer_cast<CObservationGPS>(o);
-
-                    }
-                    else if(IS_CLASS(o, CObservationIMU))
-                    {
-                        /// IMU publishing of messages happens here
-                        CObservationIMU::Ptr obsImu = std::dynamic_pointer_cast<CObservationIMU>(o);
-                    }
-                    else if(IS_CLASS(o, CObservationRange))
-                    {
-                        /// Range publishing of messages happens here
-
-                        CObservationRange::Ptr obsRng = std::dynamic_pointer_cast<CObservationRange>(o);
-                    }
-                }break;
-                case CRawlog::etActionCollection:
-                {
-                    cout << "etActionCollection " << i << endl;
-                    cout << " type : etAC " << dataset.getType(i) << " index: " << i << endl;
-                    break;
-                }
-                default:
-                    cout << " I am in default block" << endl;
-            }
+    cout << message2.field_of_view;
 
 
-
-        }// end of the try block
-        catch (exception& e)
-        {
-            cout << " CATCH CATCH CATCH BLOCK %$#@%($@*%(&@(%&@(%@(#$%(@#^$%" << endl;
-            errorMsg = e.what();
-            break;
-        }
-
-    } // end of for loop
-
-    while(ros::ok())
-    {
-        ROS_INFO("iNSIDE WHILE \n");
-        ros::spinOnce();
-    }
 
     cout << " Hello" << endl;
     return 0;
