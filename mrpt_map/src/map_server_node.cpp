f76d8fbbb8e2d42b4239258d7506b2138fe35fc7
Inounx <inounx@gmail.com>
2019-04-01 17:11:03 +0200

    Added check for MRPT version to keep older MRPT version compatibility

--- mrpt_map/src/map_server_node.cpp
+++ mrpt_map/src/map_server_node.cpp
@@ -95,7 +95,11 @@ void MapServer::init()
 			grid.getSizeX(), grid.getSizeY(), grid.getResolution(),
 			grid.getXMin(), grid.getYMin(), grid.getXMax(), grid.getYMax());
 
+#if MRPT_VERSION >= 0x199
 	mrpt_bridge::convert(*metric_map_->mapByClass<COccupancyGridMap2D>(), resp_ros_.map);
+#else
+	mrpt_bridge::convert(*metric_map_->m_gridMaps[0], resp_ros_.map);
+#endif
 	if (debug_)
 		printf(
 			"msg:         %i x %i @ %4.3fm/p, %4.3f, %4.3f, %4.3f, %4.3f\n",
