db0531f03b3f101003c63cd8c99977150e801fea
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2024-07-15 23:24:33 +0200

    add basic astar launch demo

---
+++ mrpt_tutorials/launch/demo_astar_planner_gridmap.launch.py
@@ -0,0 +1,68 @@
+# ROS 2 launch file for example in mrpt_tutorials
+#
+# See repo online: https://github.com/mrpt-ros-pkg/mrpt_navigation
+#
+
+from launch import LaunchDescription
+from launch.substitutions import TextSubstitution
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from ament_index_python import get_package_share_directory
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import os
+
+
+def generate_launch_description():
+    tutsDir = get_package_share_directory("mrpt_tutorials")
+    # print('tutsDir       : ' + tutsDir)
+
+    mrpt_astar_planner_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('mrpt_tps_astar_planner'), 'launch',
+            'tps_astar_planner.launch.py')]),
+        launch_arguments={
+            'topic_goal_sub': '/goal_pose',
+            'show_gui': 'True',
+            'topic_obstacles_gridmap_sub': '/mrpt_map/map_gridmap',
+            'topic_static_maps': '/mrpt_map/map_gridmap',
+            'topic_wp_seq_pub': '/waypoints',
+        }.items()
+    )
+
+    mrpt_map_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('mrpt_map_server'), 'launch',
+            'mrpt_map_server.launch.py')]),
+        launch_arguments={
+            'map_yaml_file': os.path.join(tutsDir, 'maps', 'demo_world2.yaml'),
+        }.items()
+    )
+
+    mvsim_node = Node(
+        package='mvsim',
+        executable='mvsim_node',
+        name='mvsim',
+        output='screen',
+        parameters=[
+            # os.path.join(tutsDir, 'params', 'mvsim_ros2_params.yaml'),
+            {
+                "world_file": os.path.join(tutsDir, 'mvsim', 'demo_world2.world.xml'),
+                "do_fake_localization": True,
+                "show_gui": True,
+            }]
+    )
+
+    rviz2_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=[
+                '-d', [os.path.join(tutsDir, 'rviz2', 'gridmap.rviz')]]
+    )
+    return LaunchDescription([
+        mrpt_map_launch,
+        mrpt_astar_planner_launch,
+        mvsim_node,
+        rviz2_node
+    ])
