6a5baafe6d1ff6b9bde2184040c5891efb5a8f55
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2025-05-28 10:38:22 +0200

    readme: add kilted badges

--- scripts/generate_statuses_table.py
+++ scripts/generate_statuses_table.py
@@ -37,6 +37,7 @@ pattern_text = \
     "[![Build Status](https://build.ros2.org/job/Hbin_uJ64__{name}__ubuntu_jammy_amd64__binary/badge/icon)](https://build.ros2.org/job/Hbin_uJ64__{name}__ubuntu_jammy_amd64__binary/) | " \
     "[![Build Status](https://build.ros2.org/job/Ibin_uJ64__{name}__ubuntu_jammy_amd64__binary/badge/icon)](https://build.ros2.org/job/Ibin_uJ64__{name}__ubuntu_jammy_amd64__binary/) | " \
     "[![Build Status](https://build.ros2.org/job/Jbin_uN64__{name}__ubuntu_noble_amd64__binary/badge/icon)](https://build.ros2.org/job/Jbin_uN64__{name}__ubuntu_noble_amd64__binary/) | " \
+    "[![Build Status](https://build.ros2.org/job/Kbin_uN64__{name}__ubuntu_noble_amd64__binary/badge/icon)](https://build.ros2.org/job/Kbin_uN64__{name}__ubuntu_noble_amd64__binary/) | " \
     "[![Build Status](https://build.ros2.org/job/Rbin_uN64__{name}__ubuntu_noble_amd64__binary/badge/icon)](https://build.ros2.org/job/Rbin_uN64__{name}__ubuntu_noble_amd64__binary/) |"
 
 process_directories(path_to_search, items_to_remove, pattern_text)
