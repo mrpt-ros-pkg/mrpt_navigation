818c56e89068ebde2bc50ae6380188690f409185
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2026-05-10 20:54:39 +0200

    fix: update clang-format auxiliary tool

--- scripts/formatter.sh
+++ scripts/formatter.sh
@@ -22,4 +22,4 @@ find \
     mrpt_tps_astar_planner \
     mrpt_tutorials \
     \( -iname "*.h" -o -iname "*.hpp" -o -iname "*.cpp" -o -iname "*.c" \) \
-  -print0 | xargs -0 clang-format-14 "${MODE[@]}"
+  -print0 | xargs -0 -r -t clang-format-14 "${MODE[@]}"
